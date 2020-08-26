dd6e7d56e3a93a903cd4b22cc94c13769c6d983c
Duncan <ddycai@gmail.com>
2020-08-26 00:58:01 -0700

    Updates

---
+++ precache-manifest.4949eaec2724f73a8164e5b721fbc49f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90c31d0423548d37771b6f61536944ba",
+    "url": "/habitica-summary/index.html"
+  },
+  {
+    "revision": "693d1b5be5e0b082ada2",
+    "url": "/habitica-summary/static/css/main.7e2bffe5.chunk.css"
+  },
+  {
+    "revision": "98b9a66ea73338ec1edb",
+    "url": "/habitica-summary/static/js/2.d689b8a2.chunk.js"
+  },
+  {
+    "revision": "849657740f66d00720db130cc1e94c97",
+    "url": "/habitica-summary/static/js/2.d689b8a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "693d1b5be5e0b082ada2",
+    "url": "/habitica-summary/static/js/main.c588f8cc.chunk.js"
+  },
+  {
+    "revision": "abf349c49ee3f420750b",
+    "url": "/habitica-summary/static/js/runtime-main.7f44936e.js"
+  },
+  {
+    "revision": "63c79fb6c556ec7f7b60d84746a40137",
+    "url": "/habitica-summary/static/media/difficulty-trivial.63c79fb6.svg"
+  }
+]);
